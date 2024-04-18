e56401ccfaff01e5b8f53943cfadd91002a23956
Gazi Akter <gaziakterdev@gmail.com>
2024-04-18 14:01:54 +0600

    update style

--- pages/main.php
+++ pages/main.php
@@ -1,24 +1,21 @@
 <div class="wrapper p-20">
-
-<h2 class="content-start text-4xl py-4 font-extrabold dark:text-white border-b-4">Gazi Crud</h2>
-<button type="button" class="float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 mt-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add New Data</button>
-
+    <div class="gazi-header">
+        <h2 class="float-left content-start text-4xl py-4 font-extrabold dark:text-white">Gazi Crud</h2>
+        <button type="button" class="float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 mt-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add New Data</button>
+    </div>
 
     <div class="relative shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Product name
+                        Serial
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Color
+                        Name
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Category
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
+                        Email
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Action
@@ -27,22 +24,21 @@
             </thead>
             <tbody>
                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
-                    </th>
                     <td class="px-6 py-4">
-                        Silver
+                        01
                     </td>
                     <td class="px-6 py-4">
-                        Laptop
+                        Gazi Akter
                     </td>
                     <td class="px-6 py-4">
-                        $2999
+                        gaziro@gmail.com
                     </td>
                     <td class="px-6 py-4">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> |
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
                     </td>
                 </tr>
+                <!--
                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Microsoft Surface Pro
@@ -111,6 +107,7 @@
                         <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                     </td>
                 </tr>
+-->
             </tbody>
         </table>
     </div>
