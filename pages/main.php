15129491732bda6c90fab911ab6dc572e412def0
Gazi Akter <gaziakterdev@gmail.com>
2024-04-20 07:53:15 +0600

    add new button

--- pages/main.php
+++ pages/main.php
@@ -30,6 +30,7 @@ $dispay_alert = 'Are you sure you want to delete this item?';
     </div>
     <?php  if ($data) { ?>
     <div class="relative shadow-md sm:rounded-lg">
+        <a href="<?php echo esc_url( admin_url( 'admin.php?page=gazi-add-new-data') ); ?>" class="mb-5 inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add New Data</a>
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
